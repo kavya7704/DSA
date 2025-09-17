acb72ff2f12a80e2bad3e39560b6d94f61bac1e4
kavya7704 <139637198+kavya7704@users.noreply.github.com>
2025-09-18 00:45:15 +0530

    Create SecondLargest.py

---
+++ SecondLargest.py
@@ -0,0 +1,15 @@
+def getSecondLargest(arr):
+      largest = -1
+      second = -1
+      for i in range(0,len(arr)):
+          
+          if arr[i] > largest:
+              second = largest
+              largest = arr[i]
+              
+          elif arr[i] < largest and arr[i] > second:
+              second = arr[i]
+      
+      return second
+arr = list(map(int,input().split()))
+getSecondLargest(arr)
