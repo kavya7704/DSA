0f2b1a72f44db764a07954ee6ffcbd3be2bef24c
kavya7704 <139637198+kavya7704@users.noreply.github.com>
2025-09-18 00:46:43 +0530

    Create pushZerosToEnd.py

---
+++ pushZerosToEnd.py
@@ -0,0 +1,10 @@
+def pushZerosToEnd(arr):
+        count = 0
+        for i in range(len(arr)):
+            if arr[i] != 0 :
+                arr[i],arr[count] = arr[count],arr[i]
+                count += 1
+                
+        return arr
+arr = list(map(int,input().split()))
+pushZerosToEnd(arr)
