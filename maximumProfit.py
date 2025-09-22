f745cfecc312959d4e8e4ed08bd67579e5efefb6
kavya7704 <139637198+kavya7704@users.noreply.github.com>
2025-09-22 23:32:41 +0530

    Create maximumProfit.py

---
+++ maximumProfit.py
@@ -0,0 +1,10 @@
+def maximumProfit(prices):
+  res = 0
+  for i in range(1,len(prices)):
+      if prices[i] > prices[i-1]:
+          res += prices[i] - prices[i-1]
+      
+  return res
+
+prices = list(map(int,input().split()))
+print(maximumProfit(prices))
